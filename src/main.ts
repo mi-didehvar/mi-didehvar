c7f5b62e474144a97c26930839ef4fbcb3ced125
Mohammad <mi.didehvar@gmail.com>
2022-12-16 16:00:22 +0330

    build and deploy

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,15 @@ import './style.css'
 import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import 'uno.css'
+import generatedRoutes from 'virtual:generated-pages'; 
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: generatedRoutes,
+  });
+  
 
 const app = createApp(App)
+app.use(router)
 
 app.mount('#app')
